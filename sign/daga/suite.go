590d306c8e802b5366897eab2da81fe348708ac1
Lucas Pires <lucas.pires@epfl.ch>
2018-10-11 14:27:09 +0200

    polish suite.go

--- sign/daga/suite.go
+++ sign/daga/suite.go
@@ -13,30 +13,31 @@ import (
 	"reflect"
 )
 
+// SuiteEC is the EC crypto concrete implementation of the DAGA Suite interface,
+// it is used to implement DAGA on the twisted Edwards curve that is birationally equivalent to Curve25519
+// (i.e. the suite uses the same curve that is used in Ed25519's EdDSA signature scheme)
+// TODO there are naming issues related to the curves in Kyber => create discussion
 type suiteEC struct {
-	edwards25519.Curve  // twisted edwards curve used in Ed25519 (birationnaly equivalent to Curve25519 <== FIXME naming issue in kyber, the Curve25519 of kyber is the same as edwards25519 only allowing vartime ops
+	edwards25519.Curve
 }
 
+// Returns a new Suite backed by a suiteEC
 func NewSuiteEC() Suite {
-	// QUESTION TODO ask if it is useful + return T or *T in interface var , what are the best practises and pitfalls to avoid ??
 	return new(suiteEC)
 }
 
-func (s suiteEC) newKey() kyber.Scalar {
-	return s.Curve.NewKey(s.RandomStream())
-}
-
-/* returns new hash.Hash computing the SHA-256 checksum
-	this hash is used in DAGA to derive valid Scalars of the group used
-	// TODO doc
- */
+// returns new hash.Hash computing the SHA-256 checksum
+// this hash is used in DAGA to derive valid Scalars of the group used
 func (s suiteEC) Hash() hash.Hash {
+	// FIXME QUESTION are length extension attacks considered to be feasible on sha256 and should we care (we don't use it to build MAC's then...) ?
+	// QUESTION maybe instead use sha512/256 ? (which should be faster on 64 bit architectures)
+	// and finally see the Hash related comment on Suite
 	return sha256.New()
 }
 
 func (s suiteEC) hashTwo() hash.Hash {
-	// FIXME
-	return nil
+	// QUESTION same as above
+	return sha256.New()
 }
 
 func (s suiteEC) RandomStream() cipher.Stream {
@@ -44,12 +45,8 @@ func (s suiteEC) RandomStream() cipher.Stream {
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// QUESTION as an alternative to current construction SuiteProof, can just embedd edward's suite in suiteEC and avoid code duplication that defines everything but I'm not fan of take everything even if not needed... ?
-// QUESTION I think my solution is more flexible too but maybe overkill, the last and best alternative is to clean other packages to decouple the functionality that they internally need from the functionality that they need but that is dictated by the user code
-// QUESTION ok I have no choice in fact, marshalling is internal to group.... why ?? => ok possible but now there is a little code duplication (New())
-// QUESTION TODO remove the "hack"/addition on suite, ask if possible to decouple the functions that are only needed internally by proof framework and the ones that come from user of framework
-// QUESTION TODO to avoid bringing the dependencies of proof into "user" code
-// used to give to the proof framework the method it needs, satisfy both proof.Suite and daga.Suite
+
+// used to give to the kyber.proof framework/package the methods it needs to operate, satisfy both proof.Suite and daga.Suite
 type SuiteProof struct {
 	Suite
 }
@@ -58,29 +55,26 @@ func newSuiteProof(suite Suite) SuiteProof {
 	return SuiteProof{suite}
 }
 
-func (s SuiteProof) Hash() hash.Hash {
-	return s.Hash()
-}
-
 // XOF returns an XOF which is implemented via the Blake2b hash.
 func (s SuiteProof) XOF(key []byte) kyber.XOF {
 	return blake2xb.New(key)
 }
 
 func (s SuiteProof) Write(w io.Writer, objs ...interface{}) error {
-	// TODO choose
+	// TODO/QUESTION what codec to choose ?
 	return fixbuf.Write(w, objs)
 }
 
 func (s SuiteProof) Read(r io.Reader, objs ...interface{}) error {
-	// TODO choose
+	// TODO/QUESTION what codec to choose ?
 	return fixbuf.Read(r, s, objs...)
 }
 
-// New implements the kyber.Encoding interface
+// New implements the kyber.Encoding interface, needed to satisfy the kyber.Proof.Suite interface
 func (s SuiteProof) New(t reflect.Type) interface{} {
-	// QUESTION FIXME not sure this is working, but a quick go playground hints it is ok.. https://play.golang.org/p/pkcd2RzlZad
+	// QUESTION FIXME not totally sure if this is working, but a quick go playground hints it is ok.. https://play.golang.org/p/pkcd2RzlZad
 	// TODO if this is ok, this implementation might be better that the one used in group/internal/marshalling/marshal.go
+	// TODO (and to my current understanding completely equivalent...only no need to have those package vars only to get their reflect type)
 	scalarInterface := reflect.TypeOf((*kyber.Scalar)(nil)).Elem()
 	pointInterface := reflect.TypeOf((*kyber.Point)(nil)).Elem()
 	if t.Implements(scalarInterface) {
@@ -93,11 +87,30 @@ func (s SuiteProof) New(t reflect.Type) interface{} {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // TODO if time, concrete implementation that uses same primitives that in DAGA paper (work in a schnorr group)
+// TODO QUESTION did someone already implemented a kyber Schnorr group somewhere ?
 //type SuiteSchnorr struct {
 //	mod.Int
 //}
 //
-//func newSuiteSchnorr() Suite {
-//	return new(SuiteSchnorr)
+//func (s SuiteSchnorr) Hash() hash.Hash {
+//	return sha256.New()
 //}
-
+//
+//func (s SuiteSchnorr) NewKey(random cipher.Stream) kyber.Scalar {
+//	return s.Scalar().Pick(random)
+//}
+//
+//func (s SuiteSchnorr) Scalar() kyber.Scalar {
+//	return mod.NewInt(nil, s.)
+//}
+//
+//func (s SuiteSchnorr) Point() kyber.Point {
+//	return nil
+//}
+//
+//func () Base() kyber.Point {
+//}
+//
+////func newSuiteSchnorr() Suite {
+////	return new(SuiteSchnorr)
+////}
